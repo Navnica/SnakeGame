3685e441d0538fc16c49bd40057b50867e7869ff
TruEnot <vadiccrut@gmail.com>
2020-07-29 02:44:28 +0400

    1.3

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ import pkg_resources.py2_warn
 pygame.init()
 
 def setDefault():
-    global snake,fruits,side,objects,fps,score,gameOver,run
+    global snake,fruits,side,objects,fps,score,gameOver,run,fruitColors
 
     pygame.display.set_caption('SnakeGame by TruEnot')
 
     snake = {
-        'color': (0, 255, 0),
-        0: [resolution[0] // 2 - 10, resolution[1] // 2 - 10],
+        0 : [resolution[0] // 2 - 10,resolution[1] // 2 - 10]
     }
 
     fruits = {
-        'color': (255, 0, 0),
         'keys': []
     }
 
+    fruitColors = {
+        0 : (255,0,0),
+        1 : (255,255,0),
+        2 : (0,0,255)
+    }
+
     side = 'up'
-    objects = [snake, fruits]
     fps = 15
     score = 1
     gameOver = False
@@ -29,12 +32,15 @@ def setDefault():
 def drawAll():
     screen.fill((0, 0, 0))
 
-    for obj in objects:
-        for pad in obj:
-            if not str(pad).isdigit():
-                continue
+    for obj in snake:
+        pygame.draw.rect(screen,(0,255,0),(snake[obj][0],snake[obj][1],10,10))
 
-            pygame.draw.rect(screen, obj['color'], (obj[pad][0], obj[pad][1], 10, 10))
+    for fruit in fruits:
+        if not str(fruit).isdigit():
+            continue
+
+        pygame.draw.rect(screen,fruits[fruit]['color'],(fruits[fruit]['position'][0],fruits[fruit]['position'][1],10,10))
+
 
     pygame.display.update()
 
@@ -50,10 +56,10 @@ def snakeMove():
         snake.update({0 : [snake[0][0] + sides[side] * 10,snake[0][1]]})
 
     for pad in snake:
-        if pad == 'color' or pad == 0:
+        if pad == 0:
             continue
 
-        snake[pad] = oldSnake[pad-1]
+        snake[pad] = oldSnake[pad - 1]
 
 def fruitGenerate():
     if gameOver:
@@ -67,12 +73,13 @@ def fruitGenerate():
     x += 10 - x % 100 % 10
     y += 10 - y % 100 % 10
 
-    if len(fruits) == 2 and chance in range(50,55) or len(fruits) in range(3,5) and chance == 50:
+    if len(fruits) == 1 and chance in range(50,55) or len(fruits) in range(2,4) and chance == 50:
         while True:
             fruitID = random.randint(0,10**10)
+            fruitColor = fruitColors[random.randint(0,2)]
             if not fruitID in fruits['keys']:
                 fruits['keys'].append(fruitID)
-                fruits.update({fruitID : [x,y]})
+                fruits.update({fruitID : {'position' : [x,y],'color' : fruitColor}})
                 break
 
 
@@ -106,9 +113,6 @@ while run:
     fruitGenerate()
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_1]:
-        fps = 1
-
     if keys[pygame.K_r]:
         setDefault()
     elif keys[pygame.K_UP]:
@@ -124,9 +128,9 @@ while run:
         if not str(fruit).isdigit():
             continue
 
-        if snake[0] == fruits[fruit]:
+        if snake[0] == fruits[fruit]['position']:
             fruits.pop(fruit)
-            snake.update({len(snake) - 1 : [resolution[0]+10,resolution[1]+10]})
+            snake.update({len(snake) : [resolution[0]+10,resolution[1]+10]})
             score += 1
             fps = 15 + score // 10
 
@@ -139,7 +143,8 @@ while run:
             continue
 
         if snake[0] == snake[pad]:
-            gameOver: bool = True
+            gameOver = True
+
 
     if snake[0][0] > resolution[0] - 1:
         snake[0][0] = 0
@@ -152,6 +157,5 @@ while run:
 
     if snake[0][1] < 0:
         snake[0][1] = resolution[1] - 10
-
 
 pygame.quit()
